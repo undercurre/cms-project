d4b26d5d3cec0d8352f33beefeed677d020f5683
lirh42 <undercurre@163.com>
2023-12-13 19:36:14 +0800

    feat: 成功部署

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,7 @@ import { Task } from './tasks/task.entity';
       port: 3306,
       username: 'root',
       password: '123456',
-      database: 'cms',
+      database: 'organizeme',
       entities: [User, Task],
     }),
     ServeStaticModule.forRoot({
