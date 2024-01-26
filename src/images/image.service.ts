32cda5ae03d9420aca113dbcb44bf0a663c90d3e
lirh42 <undercurre@163.com>
2024-01-26 11:02:16 +0800

    feat: 完成image模块

--- src/images/image.service.ts
+++ src/images/image.service.ts
@@ -5,8 +5,7 @@ import { Image } from './image.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { createWriteStream } from 'fs';
 import * as pathNode from 'path';
-import { extname } from 'path';
-import fs from 'fs';
+import * as fs from 'fs';
 
 @Injectable()
 export class ImageService {
@@ -27,20 +26,26 @@ export class ImageService {
     image.userId = id;
 
     // Generate a unique filename for the uploaded file
-    const uniqueFilename = uuidv4() + extname(file.originalname);
+    const uniqueFilename = uuidv4() + pathNode.extname(file.originalname);
 
     // Write the file to disk
-    const uploadDir = pathNode.join(__dirname, 'uploads');
+    const uploadDir = pathNode.join(__dirname, '../../', 'uploads');
+    console.log(uploadDir);
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir);
     }
-    const absolutePath = pathNode.join(__dirname, 'uploads', uniqueFilename);
+    const absolutePath = pathNode.join(
+      __dirname,
+      '../../',
+      'uploads',
+      uniqueFilename,
+    );
     const writeStream = createWriteStream(absolutePath);
     writeStream.write(file.buffer);
     writeStream.end();
 
     // Set the URL of the image
-    image.image_url = `http://localhost:3000/uploads/${uniqueFilename}`;
+    image.image_url = `http://81.71.85.68:9111/uploads/${uniqueFilename}`;
 
     // Save the image to the database
     return await this.imageRepository.save(image);
